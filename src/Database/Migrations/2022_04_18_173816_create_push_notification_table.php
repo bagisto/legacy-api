c77cc54a3f204365e932f59167087f1c53879f49
rachana chaturvedi <rachana.chaturvedi809@webkul.in>
2023-10-05 15:00:11 +0530

    table name changed

--- src/Database/Migrations/2022_04_18_173816_create_push_notification_table.php
+++ src/Database/Migrations/2022_04_18_173816_create_push_notification_table.php
@@ -13,17 +13,8 @@ return new class extends Migration
      */
     public function up()
     {
-        if (! Schema::hasTable('push_notification')) {
-            Schema::create('push_notification', function (Blueprint $table) {
-                $table->integer('id');
-                $table->string('type');
-                $table->string('image')->nullable();
-                $table->string('product_category_id')->nullable();
-                $table->boolean('status')->default(0);
-            });
-        }
-
-        Schema::table('push_notification', function (Blueprint $table) {
+        Schema::create('push_notification', function (Blueprint $table) {
+            $table->integer('id');
             $table->string('type');
             $table->string('image')->nullable();
             $table->string('product_category_id')->nullable();
